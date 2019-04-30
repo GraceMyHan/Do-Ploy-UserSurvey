e400453c8f21acbb1ced56bb5ae6f5dbd711a479
MengyuHAN <mh3881@columbia.edu>
2019-04-30 14:53:45 -0400

    add script

---
+++ init-script.sh
@@ -0,0 +1,78 @@
+
+#!/bin/sh
+# default settings
+os='centos'
+
+# extract param from instructions
+while getopts "o:" OPT
+do
+    case $OPT in
+        o)
+            os=$OPTARG
+            ;;
+    esac
+done
+
+if [ $os == 'centos' ]; then
+    sudo yum -y install yum-utils \
+    device-mapper-persistent-data \
+    lvm2
+
+    sudo yum-config-manager \
+        --add-repo \
+        https://download.docker.com/linux/centos/docker-ce.repo
+
+    sudo yum install docker-ce
+    sudo systemctl start docker
+
+elif [ $os == 'ubuntu' ]; then
+    sudo apt-get update
+
+    sudo apt-get -y install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+    sudo apt-key fingerprint 0EBFCD88
+
+    sudo add-apt-repository \
+    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) \
+    stable"
+
+    sudo apt-get update
+
+    sudo apt-get -y install docker-ce
+
+elif [ $os == 'debian' ]; then
+    sudo apt-get update
+
+    sudo apt-get -y install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg2 \
+    software-properties-common
+
+    curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+
+    sudo apt-key fingerprint 0EBFCD88
+
+    sudo add-apt-repository \
+    "deb [arch=amd64] https://download.docker.com/linux/debian \
+    $(lsb_release -cs) \
+    stable"
+
+    sudo apt-get update
+
+    sudo apt-get -y install docker-ce
+
+fi
+
+# install docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
